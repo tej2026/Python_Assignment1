bb1f0a0b562898ee99911f65e50b3ee0accb4b87
Tej Prakash Soni <tejprakash.aman@gmail.com>
2026-01-12 03:16:24 +0530

    Task2 file - discrition added

---
+++ TASK2.py
@@ -0,0 +1,6 @@
+"""Task 2: Create a Personalized Greeting
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name."""
+
